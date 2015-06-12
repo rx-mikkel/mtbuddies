332da28d36867f187b0ddac2fce70224a16e6a2a
rx-mikkel <mikkeloerum@gmail.com>
2015-06-12 20:29:26 +0200

    initial vs commit

---
+++ mtbuddies/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace mtbuddies
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/apps").Include(
+                "~/Scripts/app.js"));                 
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/css/main.css",
+                "~/Content/_reset.scss",
+                "~/Content/_layout.scss"));
+        }
+    }
+}
---
+++ mtbuddies/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mtbuddies.MvcApplication" Language="C#" %>
