5b8cd4878949da79a76e4df86c8b083f81dd4700
peteThomsen <chinapete@gmail.com>
2015-06-14 20:01:36 +0200

    Revert "Revert "Added DomainModel layer and created an entity for Tracks.""
    This reverts commit c97f8f3346a3f1d6bab53f62065c6c9db3b90447.

---
+++ DomainModels/Tracks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainModels
+{
+    public class Tracks
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Map { get; set; }
+        public string Length { get; set; }
+        public string Difficulty { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Description { get; set; }
+    }
+}
