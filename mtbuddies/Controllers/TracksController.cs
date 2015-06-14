c6a9b6c823c3dc231c7fd2bc024f750b0cbd0e31
peteThomsen <chinapete@gmail.com>
2015-06-14 21:14:37 +0200

    Added TrackController and a dbContext. Still work in progres.

---
+++ mtbuddies/Controllers/TracksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DomainModels;
+using mtbuddies.Models;
+
+namespace mtbuddies.Controllers
+{
+    public class TracksController : Controller
+    {
+        private mtbuddiesContext db = new mtbuddiesContext();
+
+        public ActionResult GetAllTrackDetails(long trackId)
+        {
+            Tracks track = db.Tracks.Where(x => x.Id == trackId).SingleOrDefault();
+            
+            return Json(new { trackDetails = track });
+        }
+    }
+}
