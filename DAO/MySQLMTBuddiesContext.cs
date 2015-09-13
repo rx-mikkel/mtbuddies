3ef5dac465ed9d00f7d4359e573a12e7e4ce13ef
peteThomsen <chinapete@gmail.com>
2015-09-13 20:29:32 +0200

    A bit of code clean up and added a mySQL db Context.

--- DAO/MySQLMTBuddiesContext.cs
+++ DAO/MySQLMTBuddiesContext.cs
@@ -1,16 +1,22 @@
 ﻿using DomainModels;
 using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DAO
 {
-    public class MTBuddiesContext : DbContext
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class MySQLMTBuddiesContext: DbContext
     {           
         public virtual DbSet<Track> Tracks { get; set; }
         public virtual DbSet<Ride> Rides { get; set; }
         public virtual DbSet<Participant> Participants { get; set; }
 
-        public MTBuddiesContext()
+        public MySQLMTBuddiesContext()
         {
 
         }
--- mtbuddies/App_Start/BundleConfig.cs
+++ mtbuddies/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace mtbuddies
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             BundleTable.EnableOptimizations = false;
